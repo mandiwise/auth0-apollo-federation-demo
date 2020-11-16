3edbc6ce7a0a487e1035c0427875bf812b9be435
Mandi Wise <hello@mandiwise.com>
2020-11-16 16:12:38 -0700

    Update AC-related imports.

--- client/src/apollo.js
+++ client/src/apollo.js
@@ -1,4 +1,12 @@
-import { ApolloClient, HttpLink, InMemoryCache } from "@apollo/client";
+import {
+  ApolloClient,
+  HttpLink,
+  InMemoryCache,
+  ApolloProvider
+} from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
+import { useAuth0 } from "@auth0/auth0-react";
+import { useEffect, useState } from "react";
 
 const cache = new InMemoryCache();
 
