c3fbfb50550809434acf1f98d183198899fb7639
Mandi Wise <hello@mandiwise.com>
2020-11-16 16:23:48 -0700

    Add access token to headers when creating AC.

--- client/src/apollo.js
+++ client/src/apollo.js
@@ -10,12 +10,23 @@ import { useEffect, useState } from "react";
 
 const cache = new InMemoryCache();
 
-function createApolloClient() {
+function createApolloClient(accessToken) {
+  const authLink = setContext((request, { headers }) => {
+    return {
+      headers: {
+        ...headers,
+        ...(accessToken && {
+          Authorization: `Bearer ${accessToken}`
+        })
+      }
+    };
+  });
+
   const httpLink = new HttpLink({
     uri: process.env.REACT_APP_GRAPHQL_API_URL
   });
 
-  return new ApolloClient({ cache, link: httpLink });
+  return new ApolloClient({ cache, link: authLink.concat(httpLink) });
 }
 
 const client = createApolloClient();
