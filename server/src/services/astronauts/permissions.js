70c676490bb3438bda4cdd50902e2c8334dcc643
Mandi Wise <hello@mandiwise.com>
2020-11-16 21:12:35 -0700

    Apply authorization rules to astronauts schema.

--- server/src/services/astronauts/permissions.js
+++ server/src/services/astronauts/permissions.js
@@ -3,3 +3,12 @@ const { rule, shield } = require("graphql-shield");
 const isAuthenticated = rule()((parent, args, { user }, info) => {
   return user !== null;
 });
+
+const permissions = shield({
+  Query: {
+    astronaut: isAuthenticated,
+    astronauts: isAuthenticated
+  }
+});
+
+module.exports = permissions;
